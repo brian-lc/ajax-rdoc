f27242f1a4beef4d1788d48356024e3c03855e28
John Nunemaker <nunemaker@gmail.com>
2008-06-30 16:13:31 -0400

    pimped the ajax template to use the quicksilver string ranking algorithm if an exact match is not found

--- rdoc/generators/template/ajax/ajax.rb
+++ rdoc/generators/template/ajax/ajax.rb
@@ -1038,6 +1038,7 @@ function getSavedSearch(tab_name){
 function setListScrollerContent(s){
 	
 	$('listScroller').innerHTML = s;
+	QSAddition.buildCache();
 }
 
 function setMainContent(s){
@@ -1209,8 +1210,8 @@ function scrollToName(searcher_name){
 
 	var scroller = $('listScroller');
 	var a_array = scroller.getElementsByTagName('a');
-
-	if (!searcher_name.match(new RegExp(/\\s+/))){ //if searcher name is blank
+	
+	if (!searcher_name.match(new RegExp(/\s+/))){ //if searcher name is blank
 		
 		//var searcher_pattern = new RegExp("^"+searcher_name, "i"); //the "i" is for case INsensitive
 		var searcher_pattern = new RegExp("^"+RegExp.escape(searcher_name.escapeHTML()), "i"); //the "i" is for case INsensitive
@@ -1232,10 +1233,16 @@ function scrollToName(searcher_name){
 		// // an attempt at binary searching... have not given up on this yet...
 		//found_index = binSearcher(searcher_pattern, a_array, 0, a_array.length);
 
-		if ((found_index >= 0) && (found_index < a_array.length)){
-
+		if ((found_index >= 0) && (found_index < a_array.length)) {
 			scrollListToAnchor(a_array[found_index].id);//scroll to the item
 			setListActiveAnchor(a_array[found_index].id);//highlight the item
+		} else {
+			var result = QSAddition.searchCache(searcher_name);
+			if (result) {
+			  if (window.console && window.console.log) { console.log('searching with qs'); }
+				scrollListToAnchor(result);
+				setListActiveAnchor(result);
+			}
 		}
 	}else{ //since searcher name is blank 
 		//scrollListToIndex(a_array, 0);//scroll to the item
@@ -1390,6 +1397,128 @@ function jsHref(url){
 //			return(split_point);
 //	
 //}
+
+var QSAddition = {	
+	buildCache: function() {
+	  if (window.console && window.console.log) { console.log('building cache'); }
+	  QSAddition.rows = $A([]);
+	  QSAddition.cache = $A([]);
+		$$('#listScroller li a').each(function(element) {
+			QSAddition.rows.push(element);
+			QSAddition.cache.push(element.innerHTML.toLowerCase());
+		});
+	},
+	
+	searchCache: function(term) {
+		var term = term.toLowerCase();
+		var len = QSAddition.cache.length;
+		var scores = $A([]);
+		
+		for(var i=0; i < len; i++) {
+			var score = QSAddition.cache[i].score(term);
+			if (score > 0) { scores.push([score, i]); }
+		}
+		
+		sorted = scores.sort(function(a,b) { return b[0] - a[0]; })
+		return sorted.length > 0 ? QSAddition.rows[sorted[0][1]] : null;
+	}
+}
+
+// qs_score - Quicksilver Score
+// 
+// A port of the Quicksilver string ranking algorithm
+// 
+// "hello world".score("axl") //=> 0.0
+// "hello world".score("ow") //=> 0.6
+// "hello world".score("hello world") //=> 1.0
+//
+// Tested in Firefox 2 and Safari 3
+//
+// The Quicksilver code is available here
+// http://code.google.com/p/blacktree-alchemy/
+// http://blacktree-alchemy.googlecode.com/svn/trunk/Crucible/Code/NSString+BLTRRanking.m
+//
+// The MIT License
+// 
+// Copyright (c) 2008 Lachie Cox
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+
+String.prototype.score = function(abbreviation,offset) {
+  offset = offset || 0 // TODO: I think this is unused... remove
+ 
+  if(abbreviation.length == 0) return 0.9
+  if(abbreviation.length > this.length) return 0.0
+
+  for (var i = abbreviation.length; i > 0; i--) {
+    var sub_abbreviation = abbreviation.substring(0,i)
+    var index = this.indexOf(sub_abbreviation)
+
+
+    if(index < 0) continue;
+    if(index + abbreviation.length > this.length + offset) continue;
+
+    var next_string       = this.substring(index+sub_abbreviation.length)
+    var next_abbreviation = null
+
+    if(i >= abbreviation.length)
+      next_abbreviation = ''
+    else
+      next_abbreviation = abbreviation.substring(i)
+ 
+    var remaining_score   = next_string.score(next_abbreviation,offset+index)
+ 
+    if (remaining_score > 0) {
+      var score = this.length-next_string.length;
+
+      if(index != 0) {
+        var j = 0;
+
+        var c = this.charCodeAt(index-1)
+        if(c==32 || c == 9) {
+          for(var j=(index-2); j >= 0; j--) {
+            c = this.charCodeAt(j)
+            score -= ((c == 32 || c == 9) ? 1 : 0.15)
+          }
+
+          // XXX maybe not port this heuristic
+          // 
+          //          } else if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:matchedRange.location]]) {
+          //            for (j = matchedRange.location-1; j >= (int) searchRange.location; j--) {
+          //              if ([[NSCharacterSet uppercaseLetterCharacterSet] characterIsMember:[self characterAtIndex:j]])
+          //                score--;
+          //              else
+          //                score -= 0.15;
+          //            }
+        } else {
+          score -= index
+        }
+      }
+   
+      score += remaining_score * next_string.length
+      score /= this.length;
+      return score
+    }
+  }
+  return 0.0
+}
 API_GREASE_START
 
 PROTOTYPE_JS = <<PROTOTYPE_JS_START
